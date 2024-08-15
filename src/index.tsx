dc9dd5f53d0e9d437f0c8aa968b6ee41ae67f8c5
Kian Malakooti <kianjmalakooti@gmail.com>
2024-08-15 15:42:41 -0700

    feat: Apollo Client

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import client from "./ApolloClient";
+import { ApolloProvider } from "@apollo/client";
+import { AuthProvider } from "./context/context";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <ApolloProvider client={client}>
+        <App />
+      </ApolloProvider>
+    </AuthProvider>
   </React.StrictMode>
 );
 
---
+++ src/mutations/mutations.ts
@@ -0,0 +1,21 @@
+import { gql } from "@apollo/client";
+
+export const REGISTER_USER = gql`
+  mutation RegisterUser($registerInput: RegisterInput) {
+    registerUser(registerInput: $registerInput) {
+      email
+      password
+      token
+    }
+  }
+`;
+
+export const LOGIN_USER = gql`
+  mutation LoginUser($loginInput: LoginInput) {
+    loginUser(loginInput: $loginInput) {
+      email
+      password
+      token
+    }
+  }
+`;
