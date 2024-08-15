b90eaf1f70633899e0bde816cfe954d28ca23cf5
Kian Malakooti <kianjmalakooti@gmail.com>
2024-08-15 15:43:09 -0700

    feat: clients page

--- src/pages/ClientsPage.tsx
+++ src/pages/ClientsPage.tsx
@@ -1,7 +1,43 @@
+import { useState } from "react";
+import ClientTable from "../components/site/ClientTable";
+import { useQuery } from "@apollo/client";
+import { getAllClients } from "../queries/queries";
+import ClientGrid from "../components/site/ClientGrid";
+import GridListButton from "../components/site/GridListButton";
+
 export default function ClientsPage() {
+  const { loading, error, data } = useQuery(getAllClients);
+
+  const [isGrid, setIsGrid] = useState(false);
+  const handleViewChange = (isGridView: boolean) => {
+    setIsGrid(isGridView);
+  };
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+
+  const clients = data.getAllClients.map((client) => ({
+    name: client.name,
+    link: client.link,
+    accessCode: client.accessCode,
+    location: client.location,
+    date: client.date,
+    hasPaid: client.hasPaid,
+    photoCount: client.photoCount || 0,
+  }));
+
   return (
     <div>
       <h1>Clients</h1>
+      <div className="flex w-full mt-1 justify-between px-4">
+        {/* <ClientFilterButtons /> */}
+        <GridListButton onChange={handleViewChange} />
+      </div>
+      {isGrid ? (
+        <ClientGrid clients={clients} />
+      ) : (
+        <ClientTable clients={clients} />
+      )}
     </div>
   );
 }
