605fb9fe32e23459cb01cb64728efae1277f9e4f
Kian Malakooti <kianjmalakooti@gmail.com>
2024-08-16 00:27:42 -0700

    feat: mock client data

---
+++ src/utils/mock.ts
@@ -0,0 +1,36 @@
+// src/data/mockClients.ts
+
+export const mockClients = [
+  {
+    name: "John Doe",
+    link: "https://example.com",
+    accessCode: "1234",
+    location: "New York",
+    date: "2021-10-10",
+    hasPaid: false,
+  },
+  {
+    name: "Jane Smith",
+    link: "https://anotherexample.com",
+    accessCode: "5678",
+    location: "Los Angeles",
+    date: "2022-01-15",
+    hasPaid: true,
+  },
+  {
+    name: "Alice Johnson",
+    link: "https://yetanotherexample.com",
+    accessCode: "2303",
+    location: "Chicago",
+    date: "2023-03-22",
+    hasPaid: true,
+  },
+  {
+    name: "Jack Flur",
+    link: "https://haha.com",
+    accessCode: "9101",
+    location: "Chicago",
+    date: "2024-01-12",
+    hasPaid: true,
+  },
+];
