4c724fb7fbee4175f0cd5fd2485a93c3e215572c
Kian Malakooti <kianjmalakooti@gmail.com>
2024-08-16 00:27:15 -0700

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import { BrowserRouter as Router } from "react-router-dom";
 import Root from "./Root";
+import { ApolloProvider } from "@apollo/client";
+import client from "./ApolloClient";
+import { AuthProvider } from "./context/context";
 
 function App() {
   return (
     <div className="min-h-screen">
-      <Router>
-        <Root />
-      </Router>
+      <AuthProvider>
+        <ApolloProvider client={client}>
+          <Router>
+            <Root />
+          </Router>
+        </ApolloProvider>
+      </AuthProvider>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,26 +1,8 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import client from "./ApolloClient";
-import { ApolloProvider } from "@apollo/client";
-import { AuthProvider } from "./context/context";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
-root.render(
-  <React.StrictMode>
-    <AuthProvider>
-      <ApolloProvider client={client}>
-        <App />
-      </ApolloProvider>
-    </AuthProvider>
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<App />);
