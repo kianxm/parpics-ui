ff450238f6b26ec261caff164705cb49569b930e
Kian Malakooti <kianjmalakooti@gmail.com>
2024-08-15 17:30:25 -0700

    feat: new routes

--- src/Root.tsx
+++ src/Root.tsx
@@ -5,6 +5,8 @@ import { ROUTES } from "./routes";
 import Site from "./pages/public/Site";
 import AuthLayout from "./layouts/AuthLayout";
 import PublicLayout from "./layouts/PublicLayout";
+import CreateClientPage from "./pages/CreateClientPage";
+import ProtectedRoute from "./lib/ProtectedRoute";
 
 function lazyHelp<T extends Record<string, ComponentType<any>>>(
   fn: () => Promise<T>,
@@ -21,6 +23,7 @@ const SignUpPage = lazyHelp(() => import("./pages/public/SignUpPage"));
 const LoginPage = lazyHelp(() => import("./pages/public/LoginPage"));
 const Dashboard = lazyHelp(() => import("./pages/Dashboard"));
 const ClientsPage = lazyHelp(() => import("./pages/ClientsPage"));
+const ClientPage = lazyHelp(() => import("./pages/ClientPage"));
 
 export default function Root() {
   return (
@@ -38,20 +41,53 @@ export default function Root() {
           <Route path={ROUTES.PRICING} element={<PricingPage />} />
           <Route path={ROUTES.LOGIN} element={<LoginPage />} />
           <Route path={ROUTES.SIGN_UP} element={<SignUpPage />} />
+
           <Route
             path={ROUTES.DASHBOARD.DASHBOARD}
             element={
-              <AuthLayout>
-                <Dashboard />
-              </AuthLayout>
+              <ProtectedRoute
+                element={
+                  <AuthLayout>
+                    <Dashboard />
+                  </AuthLayout>
+                }
+              />
             }
           />
           <Route
             path={ROUTES.CLIENTS.CLIENTS}
             element={
-              <AuthLayout>
-                <ClientsPage />
-              </AuthLayout>
+              <ProtectedRoute
+                element={
+                  <AuthLayout>
+                    <ClientsPage />
+                  </AuthLayout>
+                }
+              />
+            }
+          />
+          <Route
+            path={ROUTES.CLIENTS.CREATE}
+            element={
+              <ProtectedRoute
+                element={
+                  <AuthLayout>
+                    <CreateClientPage />
+                  </AuthLayout>
+                }
+              />
+            }
+          />
+          <Route
+            path={ROUTES.CLIENTS.CLIENT}
+            element={
+              <ProtectedRoute
+                element={
+                  <AuthLayout>
+                    <ClientPage />
+                  </AuthLayout>
+                }
+              />
             }
           />
         </Routes>
