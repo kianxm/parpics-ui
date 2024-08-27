069e17d6998a7d2b106b0f304de5e49386c3d677
Kian Malakooti <kianjmalakooti@gmail.com>
2024-08-27 15:17:39 -0700

    feat: dashboard cards

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,24 +1,21 @@
 import { Button } from "../components/ui/button";
-// import { useQuery } from "@apollo/client";
-// import { getDashboardOverview } from "../queries/queries";
-// import { DashboardOverview } from "../types/client";
+import { Suspense } from "react";
+import Spinner from "../components/Spinner";
+import DashboardContent from "../components/dashboard/DashboardContent";
+import { useCurrentUser } from "../utils/useCurrentUser";
 
 export default function DashboardPage() {
-  // const { loading, error, data } = useQuery(getDashboardOverview, {
-  //   variables: { userId: user.user_id },
-  // });
-
-  // if (loading) return <p>Loading...</p>;
-  // if (error) return <p>Error: {error.message}</p>;
-
-  // const snapshot = data?.getDashboardOverview as DashboardOverview;
+  const user = useCurrentUser();
 
   return (
-    <div className="flex flex-col h-full w-full">
+    <div className="flex flex-col h-full w-full space-y-4">
       <div className="flex md:grow-0 justify-between">
         <span className="font-semibold text-2xl mt-1">Dashboard</span>
         <Button variant="outline">Download</Button>
       </div>
+      <Suspense fallback={<Spinner />}>
+        <DashboardContent userId={user.user_id} />
+      </Suspense>
     </div>
   );
 }
