52c02ebcce28225d5c8a2469dcdd008fdefed312
Kian Malakooti <kianjmalakooti@gmail.com>
2024-08-27 15:18:36 -0700

    refactor dashboard content

---
+++ src/components/dashboard/DashboardContent.tsx
@@ -0,0 +1,265 @@
+import * as React from "react";
+import {
+  ListFilter,
+  Copy,
+  CreditCard,
+  ChevronLeft,
+  ChevronRight,
+  FileIcon,
+} from "lucide-react";
+import { useCurrentUser } from "../../utils/useCurrentUser";
+import { useQuery, useSuspenseQuery } from "@apollo/client";
+import { getDashboardOverview } from "../../queries/queries";
+import { DashboardOverview } from "../../types/client";
+import { formatSize } from "../../utils/format";
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardFooter,
+  CardContent,
+} from "../ui/card";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "../ui/tabs";
+import {
+  DropdownMenu,
+  DropdownMenuCheckboxItem,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "../ui/dropdown-menu";
+import { Button } from "../ui/button";
+import { Progress } from "../ui/progress";
+import { Separator } from "../ui/separator";
+
+export default function DashboardContent({ userId }: { userId: string }) {
+  const { data } = useQuery(getDashboardOverview, {
+    variables: { userId },
+    skip: !userId,
+  });
+
+  console.log(data);
+
+  const snapshot: DashboardOverview = data?.getDashboardOverview;
+
+  const totalStorage = snapshot?.storageUsed + snapshot?.storageRemaining;
+  const percentageUsed = (snapshot?.storageUsed / totalStorage) * 100;
+
+  return (
+    <main className="grid flex-1 items-start gap-4 sm:py-0 md:gap-8 lg:grid-cols-3 xl:grid-cols-3">
+      <div className="grid auto-rows-max items-start gap-4 md:gap-8 lg:col-span-2">
+        <div className="grid gap-4 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-2 xl:grid-cols-4">
+          <Card className="sm:col-span-2">
+            <CardHeader className="pb-3">
+              <CardTitle>Your Clients</CardTitle>
+              <CardDescription className="max-w-lg text-balance leading-relaxed">
+                Create clients albums and websites with ease.
+              </CardDescription>
+            </CardHeader>
+            <CardFooter>
+              <Button>Create Client</Button>
+            </CardFooter>
+          </Card>
+          <Card>
+            <CardHeader className="pb-2">
+              <CardDescription>Storage</CardDescription>
+              <CardTitle className="text-4xl">
+                {formatSize(snapshot?.storageUsed)}
+              </CardTitle>
+            </CardHeader>
+            <CardContent>
+              <div className="text-xs text-muted-foreground">
+                You have {formatSize(snapshot?.storageRemaining)} remaining
+              </div>
+            </CardContent>
+            <CardFooter>
+              <Progress value={percentageUsed} />
+            </CardFooter>
+          </Card>
+          <Card>
+            <CardHeader className="pb-2">
+              <CardDescription>Total Clients</CardDescription>
+              <CardTitle className="text-4xl">
+                {snapshot?.totalClients}
+              </CardTitle>
+            </CardHeader>
+            <CardContent>
+              <div className="text-xs text-muted-foreground">oh gangy</div>
+            </CardContent>
+            <CardFooter>
+              <Progress value={2} />
+            </CardFooter>
+          </Card>
+        </div>
+        <Tabs defaultValue="week">
+          <div className="flex items-center">
+            <TabsList>
+              <TabsTrigger value="week">Week</TabsTrigger>
+              <TabsTrigger value="month">Month</TabsTrigger>
+              <TabsTrigger value="year">Year</TabsTrigger>
+            </TabsList>
+            <div className="ml-auto flex items-center gap-2">
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                  <Button
+                    variant="outline"
+                    size="sm"
+                    className="h-7 gap-1 text-sm"
+                  >
+                    <ListFilter className="h-3.5 w-3.5" />
+                    <span className="sr-only sm:not-sr-only">Filter</span>
+                  </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent align="end">
+                  <DropdownMenuLabel>Filter by</DropdownMenuLabel>
+                  <DropdownMenuSeparator />
+                  <DropdownMenuCheckboxItem checked>
+                    Fulfilled
+                  </DropdownMenuCheckboxItem>
+                  <DropdownMenuCheckboxItem>Declined</DropdownMenuCheckboxItem>
+                  <DropdownMenuCheckboxItem>Refunded</DropdownMenuCheckboxItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
+              <Button size="sm" variant="outline" className="h-7 gap-1 text-sm">
+                <FileIcon className="h-3.5 w-3.5" />
+                <span className="sr-only sm:not-sr-only">Export</span>
+              </Button>
+            </div>
+          </div>
+          <TabsContent value="week">
+            <Card>
+              <CardHeader className="px-7">
+                <CardTitle>Orders</CardTitle>
+                <CardDescription>
+                  Recent orders from your store.
+                </CardDescription>
+              </CardHeader>
+              <CardContent></CardContent>
+            </Card>
+          </TabsContent>
+        </Tabs>
+      </div>
+      <div>
+        <Card className="overflow-hidden">
+          <CardHeader className="flex flex-row items-start bg-muted/50">
+            <div className="grid gap-0.5">
+              <CardTitle className="group flex items-center gap-2 text-lg">
+                Notifications
+                <Button
+                  size="icon"
+                  variant="outline"
+                  className="h-6 w-6 opacity-0 transition-opacity group-hover:opacity-100"
+                >
+                  <Copy className="h-3 w-3" />
+                  <span className="sr-only">Copy Order ID</span>
+                </Button>
+              </CardTitle>
+              <CardDescription>Stay updated</CardDescription>
+            </div>
+            <div className="ml-auto flex items-center gap-1">
+              <Button size="icon" variant="outline" className="h-8 w-8">
+                <ChevronRight className="h-3.5 w-3.5" />
+              </Button>
+            </div>
+          </CardHeader>
+          <CardContent className="p-6 text-sm">
+            <div className="grid gap-3">
+              <div className="font-semibold">Order Details</div>
+              <ul className="grid gap-3">
+                <li className="flex items-center justify-between">
+                  <span className="text-muted-foreground">
+                    Glimmer Lamps x <span>2</span>
+                  </span>
+                  <span>$250.00</span>
+                </li>
+                <li className="flex items-center justify-between">
+                  <span className="text-muted-foreground">
+                    Aqua Filters x <span>1</span>
+                  </span>
+                  <span>$49.00</span>
+                </li>
+              </ul>
+              <Separator className="my-2" />
+              <ul className="grid gap-3">
+                <li className="flex items-center justify-between">
+                  <span className="text-muted-foreground">Subtotal</span>
+                  <span>$299.00</span>
+                </li>
+                <li className="flex items-center justify-between">
+                  <span className="text-muted-foreground">Shipping</span>
+                  <span>$5.00</span>
+                </li>
+                <li className="flex items-center justify-between">
+                  <span className="text-muted-foreground">Tax</span>
+                  <span>$25.00</span>
+                </li>
+                <li className="flex items-center justify-between font-semibold">
+                  <span className="text-muted-foreground">Total</span>
+                  <span>$329.00</span>
+                </li>
+              </ul>
+            </div>
+            <Separator className="my-4" />
+            <div className="grid grid-cols-2 gap-4">
+              <div className="grid gap-3">
+                <div className="font-semibold">Shipping Information</div>
+                <address className="grid gap-0.5 not-italic text-muted-foreground">
+                  <span>Liam Johnson</span>
+                  <span>1234 Main St.</span>
+                  <span>Anytown, CA 12345</span>
+                </address>
+              </div>
+              <div className="grid auto-rows-max gap-3">
+                <div className="font-semibold">Billing Information</div>
+                <div className="text-muted-foreground">
+                  Same as shipping address
+                </div>
+              </div>
+            </div>
+            <Separator className="my-4" />
+            <div className="grid gap-3">
+              <div className="font-semibold">Customer Information</div>
+              <dl className="grid gap-3">
+                <div className="flex items-center justify-between">
+                  <dt className="text-muted-foreground">Customer</dt>
+                  <dd>Liam Johnson</dd>
+                </div>
+                <div className="flex items-center justify-between">
+                  <dt className="text-muted-foreground">Email</dt>
+                  <dd>
+                    <a href="mailto:">liam@acme.com</a>
+                  </dd>
+                </div>
+                <div className="flex items-center justify-between">
+                  <dt className="text-muted-foreground">Phone</dt>
+                  <dd>
+                    <a href="tel:">+1 234 567 890</a>
+                  </dd>
+                </div>
+              </dl>
+            </div>
+            <Separator className="my-4" />
+            <div className="grid gap-3">
+              <div className="font-semibold">Payment Information</div>
+              <dl className="grid gap-3">
+                <div className="flex items-center justify-between">
+                  <dt className="flex items-center gap-1 text-muted-foreground">
+                    <CreditCard className="h-4 w-4" />
+                    Visa
+                  </dt>
+                  <dd>**** **** **** 4532</dd>
+                </div>
+              </dl>
+            </div>
+          </CardContent>
+          <CardFooter className="flex flex-row items-center border-t bg-muted/50 px-6 py-3">
+            <div className="text-xs text-muted-foreground">
+              Updated <time dateTime="2023-11-23">November 23, 2023</time>
+            </div>
+          </CardFooter>
+        </Card>
+      </div>
+    </main>
+  );
+}
